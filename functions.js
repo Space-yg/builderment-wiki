e3a16474580f08971e5ce983d44909465043bc86
Space.yg <88850324+Space-yg@users.noreply.github.com>
2022-12-14 14:04:26 +0200

    Add files via upload

--- functions.js
+++ functions.js
@@ -6,21 +6,19 @@ for (let i = location.href.split("/").length - 2; i > 0; i--) {
     if (location.href.split("/")[i] === "builderment-wiki") break;
     else path += "../";
 }
-console.log(location.href.split("/"))
-console.log(path)
+
 // Search
 function search() {
     const input = document.getElementsByTagName("input")[0];
     const searchDiv = document.getElementsByClassName("search")[0];
     const ulTag = document.getElementById("search");
-    console.log(ulTag)
     ulTag.innerHTML = "";
     if (input.value) searchDiv.style.visibility = "visible";
     else return searchDiv.style.visibility = "hidden";
     const showPages = [];
     for (let i = 0; i < pagesTags.length; i++) if (pagesTags[i].innerText.toUpperCase().indexOf(input.value.toUpperCase()) > -1) showPages.push(pagesTags[i]);
     for (let i = 0; i < showPages.length && i < 5; i++) {
-        // <li onclick="window.location.href = `./wikis/Items copy/page.html`;">Items</li>
+        // <li onclick="window.location.href = `link`;">name</li>
         const li = document.createElement("li");
         li.setAttribute("onclick", `window.location.href = "${path + showPages[i].getAttribute("href")}"`);
         li.innerHTML = showPages[i].innerText;
